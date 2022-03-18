32e2c1f31985c62f75b6dc28fb0d9a151ce14b6d
Admin <Admin@DESKTOP-7E78KSE>
2022-03-18 18:36:59 +0530

    Oops Concepts All Polymorphism Method Overloading

---
+++ OopsConcepts_Polymorphism/src/Addition.java
@@ -0,0 +1,28 @@
+
+public class Addition {
+	void add(int x, int y)
+	{
+		System.out.println("Addition is:" +(x+y));
+	}
+
+	void add(int x, int y, int z)
+	{
+		System.out.println("Addition is:" +(x+y+z));
+	}
+
+	void add(int x, int y, int z, int q)
+	{
+		System.out.println("Addition is:" +(x+y+z+q));
+	}
+
+	public static void main(String[] args) {
+		Addition object = new Addition();
+		object.add(5,5);
+		object.add(5,5,5);
+		object.add(5,5,5,5);
+				
+		
+
+	}
+
+}
