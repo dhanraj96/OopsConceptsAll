0c4fb7f2c3565f643222a64deca0db6a5fdcd95f
Admin <Admin@DESKTOP-7E78KSE>
2022-03-18 18:32:39 +0530

    Oops Concepts All Abstraction

---
+++ OopsConcepts_Abstraction1/src/Tata_Motors.java
@@ -0,0 +1,34 @@
+class Seltos
+{ 
+	
+	private int Speed;
+	private int getSpeed()
+	{
+		return Speed;
+	}
+	private void setSpeed(int speed)
+	{
+		Speed = speed;
+	}
+}
+
+public class Tata_Motors {
+int noCars;
+	String Model;
+	String engine = "3L Diesel";
+
+	public static void main(String[] args) {
+		Seltos object = new Seltos();
+		object.getSpeed();
+		object.setSpeed(100);
+	
+		
+		
+
+	
+
+	}
+
+	}
+
+
